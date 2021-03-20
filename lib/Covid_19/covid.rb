2b3a479e058df71a850eb9b8c7b94db60faecdc0
MuhammadDanyalMahdi <danyalm50@gmail.com>
2021-03-20 16:11:29 -0400

    removed minor bugs

--- lib/Covid_19/covid.rb
+++ lib/Covid_19/covid.rb
@@ -3,27 +3,7 @@ class Covid19::Covid
     attr_reader :confirmed, :recovered, :deaths, :country, :population, :continent, :location
 
     @@all = []
-
-    # puts <<-DOC
-    # 1. Afhanistan
-    # 2. Pakistan
-    # 3. India
-    # DOC
-
-    # def self.new_from_list(hash)
-    #     binding.pry
-
-    #     # country_1 = self.new
-    #     # country_1.confirmed = "45000"
-    #     # country_1.recovered = "25000"
-    #     # country_1.deaths = "20000"
-    #     # country_1.country = "Afghanistan"
-    #     # country_1.population = "23000000"
-    #     # country_1.continent = "Asia"
-    #     # country_1.location = "Southern Asia"
-    #     # [country_1]
-    # end
-
+    
     def initialize(hash)
         @confirmed = hash[1]['All']['confirmed']
         @recovered = hash[1]['All']['recovered']
