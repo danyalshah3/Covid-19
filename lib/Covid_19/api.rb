28b301c7e8ea57305bbb1affce161414d9a868e6
MuhammadDanyalMahdi <danyalm50@gmail.com>
2021-03-20 17:31:04 -0400

    adding features

--- lib/Covid_19/api.rb
+++ lib/Covid_19/api.rb
@@ -2,9 +2,10 @@ class Covid19::API
 
     attr_reader :response
 
-        def initialize
-     @response = HTTParty.get("https://covid-api.mmediagroup.fr/v1/cases")
-        #   binding.pry
-         end
+    def initialize
+            
+        @response = HTTParty.get("https://covid-api.mmediagroup.fr/v1/cases")
 
     end
+
+end
--- lib/Covid_19/cli.rb
+++ lib/Covid_19/cli.rb
@@ -6,70 +6,80 @@
         sleep(0.5)
         get_countries
         menu
-     get_country_details
-        
- end
-
- def login
-
- end
-
-
- def get_countries
-    puts ""
-    puts "Here is the list of affected countries:".colorize(:light_red)
-    puts ""
-    @lists = Covid19::Covid.all
-    @lists.each_with_index do |country, i|
-        puts "#{i+1}, #{country.country}".colorize(:green)
-        # binding.pry
-    end 
- end
-def menu
-    input = ""
-    puts "Which Country you would like to get details about:".colorize(:light_red)
-    input = gets.strip
-
-    while input != "exit" 
-        country = Covid19::Covid.find_by_name(input)
-        c = get_country_details(country)
-
-        puts ""
-        puts "Would you like to see details about another country? If yes, type a country name to view its details, type yes to see the list or type exit to leave:"
-
-        input = gets.strip
-        if input == "yes"
-            get_countries
-        elsif input == "exit"
-            puts "You are now logged out, see you tomorrow!".colorize(:light_red)
-            exit
-    
-            
-        end  
+        get_country_details   
     end
-end  
+
+
+
+    def get_countries
+        puts ""
+        puts "Gathering information"
+        sleep(1)
+        puts "Accessing Databases"
+        sleep(1)
+        puts "Making it look nice!"
+        sleep(2)
+        puts ""
+        puts "Here is the list of affected countries:".colorize(:light_red)
+        sleep (1)
+
+        @lists = Covid19::Covid.all
+        @lists.each_with_index do |country, i|
+            puts "#{i+1}, #{country.country}".colorize(:green)
+            
+        end 
+    end
+
+    def menu
+        input = ""
+        puts "Which Country you would like to get details about:".colorize(:light_red)
+        input = gets.strip
+
+        while input != "exit" 
+            country = Covid19::Covid.find_by_name(input)
+             get_country_details(country)
+
+            puts ""
+            puts "Would you like to see details about another country? Type yes to start over or type exit to leave:".colorize(:light_red)
+
+            input = gets.strip
+            if input == "yes"
+             menu
+            elsif input == "exit"
+             puts "You are now logged out, see you tomorrow!".colorize(:light_red)
+             exit
+            else     
+        
+                
+             end  
+        end
+    end  
 
 
 
 
-def logo
-    puts "Welcome to the data hub of Covid_19 cases:".colorize(:green)
-    
-end
-       
+    def logo
+        puts "Welcome to the data hub of Covid_19 cases:".colorize(:green)
+        
+    end
+        
 
     def get_country_details(country)
-        if(country != nil)
-        puts "" 
-        puts "*******#{country.country} - #{country.continent}*********#{country.location}".colorize(:green)
-        puts ""
-        puts "Population:        #{country.population}".colorize(:green)
-        puts "Confirmed:         #{country.confirmed}".colorize(:green)
-        puts "Recovered:         #{country.recovered}".colorize(:green)
-        puts "Deaths:            #{country.deaths}".colorize(:green)
-        puts "*********************************************"
-        else 
-            puts "Not sure what you are looking for, type list to see the list of school or type exit.".colorize(:light_red)
+        sleep(1)
+        if(country != nil) 
+            puts <<-DOC
+                
+            1. "#{country.country} - #{country.continent} - #{country.location}"
+            
+            2. "Population:        #{country.population}"
+            3. "Confirmed:         #{country.confirmed}"
+            4. "Recovered:         #{country.recovered}"
+            5. "Deaths:            #{country.deaths}"
+             "*********************************************"
+            DOC
+    
+        else
+            puts "Not sure what country that you are looking for;".colorize(:light_red)
         end
     end
 
